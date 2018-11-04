f39337f99b2924a13ac581e25f6d868de3f6bffb
florimondmanca <florimond.manca@gmail.com>
2018-11-04 12:38:43 +0100

    reorder views in example.py

--- example.py
+++ example.py
@@ -4,16 +4,16 @@ from bocadillo.response import Response
 api = bocadillo.API()
 
 
+@api.route('/greet/{person}')
+def greet(req, resp: Response, person: str):
+    resp.content = f'Hello, {person}!'
+
+
 @api.route('/add/{x:d}/{y:d}')
 class AddView:
 
     async def get(self, req, resp, x: int, y: int):
         resp.media = {'result': x + y}
-
-
-@api.route('/greet/{person}')
-def index(req, resp: Response, person: str):
-    resp.content = f'Hello, {person}!'
 
 
 if __name__ == '__main__':
