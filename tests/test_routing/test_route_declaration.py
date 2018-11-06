97fd5d68b94c319d83f333ee5b17a12eccef19f2
florimondmanca <florimond.manca@gmail.com>
2018-11-06 21:38:33 +0100

    reorder route declaration tests

--- tests/test_routing/test_route_declaration.py
+++ tests/test_routing/test_route_declaration.py
@@ -12,20 +12,6 @@ def test_if_route_not_registered_then_404(api: API):
     assert api.client.get('/test').status_code == 404
 
 
-def test_route_must_start_with_slash(api: API):
-    with pytest.raises(RouteDeclarationError):
-        @api.route('foo')
-        def foo(req, res):
-            pass
-
-
-def test_route_must_expect_request_and_response(api: API):
-    with pytest.raises(RouteDeclarationError):
-        @api.route('/foo/{bar}')
-        def foo(bar):
-            pass
-
-
 def test_if_route_registered_then_not_404(api: API):
     @api.route('/')
     def test(req, res):
@@ -40,3 +26,17 @@ def test_default_status_code_is_200_on_routes(api: API):
         pass
 
     assert api.client.get('/').status_code == 200
+
+
+def test_route_must_start_with_slash(api: API):
+    with pytest.raises(RouteDeclarationError):
+        @api.route('foo')
+        def foo(req, res):
+            pass
+
+
+def test_route_must_expect_request_and_response(api: API):
+    with pytest.raises(RouteDeclarationError):
+        @api.route('/foo/{bar}')
+        def foo(bar):
+            pass
