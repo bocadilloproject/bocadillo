15af2b04cad2a3a8d82b0082421a811c94408e07
florimondmanca <florimond.manca@gmail.com>
2018-11-06 21:27:30 +0100

    Add test on route parameter type validation

--- tests/test_routing/test_route_parameters.py
+++ tests/test_routing/test_route_parameters.py
@@ -10,7 +10,17 @@ def test_parameter_is_passed_as_argument(api: API):
         res.content = person
 
     response = api.client.get('/greet/John')
+    assert response.status_code == 200
     assert response.text == 'John'
+
+
+def test_if_route_expects_int_but_int_not_given_then_404(api: API):
+    @api.route('/add/{x:d}/{y:d}')
+    def add(req, res, x, y):
+        res.content = str(x + y)
+
+    response = api.client.get('/add/1/foo')
+    assert response.status_code == 404
 
 
 def test_if_parameter_not_included_in_signature_then_error_raised(api: API):
