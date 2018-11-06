ade0692271b8410925fd437192228046589c2544
florimondmanca <florimond.manca@gmail.com>
2018-11-06 18:03:24 +0100

    Add docstring for static

--- bocadillo/static.py
+++ bocadillo/static.py
@@ -5,6 +5,10 @@ from .types import WSGIApp
 
 
 def static(directory: str) -> WSGIApp:
+    """Return a WSGI app that serves static files under the given directory.
+
+    Powered by WhiteNoise.
+    """
     app = WhiteNoise(empty_wsgi_app())
     app.add_files(directory)
     return app
