48b1478e2a7015d33c07f997da8cf29c411795a0
florimondmanca <florimond.manca@gmail.com>
2018-11-06 22:57:49 +0100

    add test for ignored route methods on class-based views

--- tests/test_routing/test_route_methods.py
+++ tests/test_routing/test_route_methods.py
@@ -3,7 +3,6 @@ import pytest
 from bocadillo import API
 from bocadillo.exceptions import RouteDeclarationError
 
-
 @pytest.mark.parametrize('methods, method, expected_status_code', [
     (['get', 'post'], 'get', 200),
     (['post'], 'get', 405),
@@ -18,8 +17,32 @@ def test_allowed_methods(api: API, methods, method, expected_status_code):
     assert response.status_code == expected_status_code
 
 
+@pytest.mark.parametrize('methods, method', [
+    (['get', 'post'], 'get'),
+    (['post'], 'get'),
+    ([], 'put'),
+])
+def test_route_methods_are_ignored_on_class_based_views(
+        api: API, methods, method):
+    @api.route('/class', methods=methods)
+    class Index:
+        def get(self, req, res):
+            pass
+
+        def put(self, req, res):
+            pass
+
+    response = getattr(api.client, method)('/class')
+    assert response.status_code == 200
+
+
 def test_allowed_method_must_be_valid_http_method(api: API):
     with pytest.raises(RouteDeclarationError):
         @api.route('/', methods=['foo'])
         def index(req, res):
+            pass
+
+    with pytest.raises(RouteDeclarationError):
+        @api.route('/', methods=['bar'])
+        class Index:
             pass
