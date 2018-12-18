21ddedfd885eb6912d4c950ac63a82f5fbc7da21
florimondmanca <florimond.manca@gmail.com>
2018-12-18 12:55:36 +0100

    add test for default CORS config

--- tests/test_cors.py
+++ tests/test_cors.py
@@ -1,6 +1,23 @@
 from bocadillo import API
 
 
+def test_no_allowed_origins_by_default():
+    api = API(enable_cors=True)
+
+    @api.route("/")
+    async def index(req, res):
+        pass
+
+    response = api.client.options(
+        "/",
+        headers={
+            "origin": "foobar.com",
+            "access-control-request-method": "GET",
+        },
+    )
+    assert response.status_code == 400
+
+
 def test_if_origin_not_in_allow_origins_then_400():
     api = API(enable_cors=True, cors_config={"allow_origins": ["foobar.com"]})
 
