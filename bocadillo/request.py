f396250f7266a2f90f3b4b1c45f624f5d239e6fa
florimondmanca <florimond.manca@gmail.com>
2018-12-27 11:31:07 +0100

    return 400 if json is malformed, add tests and docs

--- bocadillo/request.py
+++ bocadillo/request.py
@@ -1,6 +1,23 @@
+from json import JSONDecodeError
+
 from starlette.requests import Request as _Request
 
+from .exceptions import HTTPError
 
-# Alias (for now)
+
 class Request(_Request):
-    pass
+    """The succulent request object."""
+
+    async def json(self):
+        """Parse the request body as JSON.
+
+        # Returns
+        json (dict): the result of `json.loads(await self.body())`.
+
+        # Raises
+        HTTPError(400): if the JSON is malformed.
+        """
+        try:
+            return await super().json()
+        except JSONDecodeError:
+            raise HTTPError(400, detail="JSON is malformed.")
