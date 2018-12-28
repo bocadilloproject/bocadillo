674339281d21d032380ddb8f12b34be24d2aaa24
florimondmanca <florimond.manca@gmail.com>
2018-12-28 01:06:49 +0100

    add test for non-existing websocket endpoint

--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -1,3 +1,6 @@
+import pytest
+from starlette.websockets import WebSocketDisconnect
+
 from bocadillo import WebSocket, API
 
 
@@ -11,3 +14,10 @@ def test_websocket_route(api: API):
     with api.client.websocket_connect("/chat") as ws_client:
         ws_client.send_text("ping")
         assert ws_client.receive_text() == "pong"
+
+
+def test_non_existing_endpoint_returns_403_as_per_the_asgi_spec(api: API):
+    with pytest.raises(WebSocketDisconnect) as ctx:
+        with api.client.websocket_connect("/foo"):
+            pass
+    assert ctx.value.code == 403
