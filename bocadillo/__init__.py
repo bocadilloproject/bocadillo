28a93ad38a349449172b5e52675394899e03d1bf
florimondmanca <florimond.manca@gmail.com>
2019-01-18 21:30:57 +0100

    rename server_sent_events module to sse

--- bocadillo/__init__.py
+++ bocadillo/__init__.py
@@ -4,7 +4,7 @@ from .media import Media
 from .middleware import Middleware
 from .recipes import Recipe
 from .staticfiles import static
-from .server_sent_events import server_event
+from .sse import server_event
 from .views import view
 from .websockets import WebSocket, WebSocketDisconnect
 from .response import Response
