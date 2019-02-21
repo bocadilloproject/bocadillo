420c34842ecef690f8217954108e3aad0e453756
florimondmanca <florimond.manca@gmail.com>
2019-02-21 13:08:23 +0100

    fix app fixture in form parsing test

--- tests/test_form_parsing.py
+++ tests/test_form_parsing.py
@@ -1,16 +1,16 @@
 from starlette.datastructures import FormData
 
-from bocadillo import API
+from bocadillo import App
 
 
-def test_form_parsing(api: API):
-    @api.route("/")
+def test_form_parsing(app: App):
+    @app.route("/")
     async def index(req, res):
         form = await req.form()
         assert isinstance(form, FormData)
         # not actually a dict, so not immediately JSON-serializable
         res.media = dict(form)
 
-    r = api.client.get("/", data={"foo": "bar"})
+    r = app.client.get("/", data={"foo": "bar"})
     assert r.status_code == 200
     assert r.json() == {"foo": "bar"}
