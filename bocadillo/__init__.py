5ebde6367087d3a2fbb25393599368c9d84f3410
florimondmanca <florimond.manca@gmail.com>
2019-03-08 10:16:18 +0100

    feat: expose provider helpers

--- bocadillo/__init__.py
+++ bocadillo/__init__.py
@@ -1,12 +1,12 @@
 from .applications import App
 from .errors import HTTPError
 from .middleware import ASGIMiddleware, Middleware
+from .providers import discover_providers, provider, useprovider
 from .recipes import Recipe
+from .request import ClientDisconnect, Request
 from .response import Response
-from .request import ClientDisconnect
-from .request import Request
-from .staticfiles import static
 from .sse import server_event
+from .staticfiles import static
 from .templates import Templates
 from .views import view
 from .websockets import WebSocket, WebSocketDisconnect
