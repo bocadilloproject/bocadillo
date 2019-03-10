9062dbfc578d9aae19640289b34bd86aaeea033d
florimondmanca <florimond.manca@gmail.com>
2019-03-10 21:03:34 +0100

    feat: add twitter card meta tags

--- docs/.vuepress/theme/SocialMeta.js
+++ docs/.vuepress/theme/SocialMeta.js
@@ -1,6 +1,34 @@
 export default {
-  created() {
-    console.log("Created!");
-    console.log(this.$page.frontmatter);
+  mounted() {
+    const { frontmatter } = this.$page;
+
+    const meta = [];
+
+    meta.push({
+      name: "twitter:title",
+      content:
+        (frontmatter.title ? frontmatter.title : this.$page.title) +
+        " | " +
+        this.$site.title
+    });
+
+    meta.push({
+      name: "twitter:description",
+      content: frontmatter.description
+        ? frontmatter.description
+        : this.$site.description
+    });
+
+    if (frontmatter.image) {
+      meta.push({ name: "twitter:image", content: frontmatter.coverImage });
+    }
+
+    const head = document.getElementsByTagName("head")[0];
+    meta.forEach(m => {
+      const tag = document.createElement("meta");
+      tag.name = m.name;
+      tag.content = m.content;
+      head.appendChild(tag);
+    });
   }
 };
