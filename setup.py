839a373aaca4bb02db1370863b278323263525ce
florimondmanca <florimond.manca@gmail.com>
2019-03-30 11:34:39 +0100

    upgrade aiodine

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
         "parse",
         "python-multipart",
         "websockets>=6.0",
-        "aiodine>=1.2.1, <2.0",
+        "aiodine>=1.2.5, <2.0",
     ],
     extras_require={"files": ["aiofiles"], "sessions": ["itsdangerous"]},
     python_requires=">=3.6",
