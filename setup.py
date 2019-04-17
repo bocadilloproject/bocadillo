3aa9acdec464063ad7721db0ed8b1ec996d336ce
Florimond Manca <florimond.manca@gmail.com>
2019-04-17 21:53:17 +0200

    add [full] extra, refactor ci pipeline (#258)

--- setup.py
+++ setup.py
@@ -11,6 +11,23 @@ GITHUB = "https://github.com/bocadilloproject/bocadillo"
 DOCS = "https://bocadilloproject.github.io"
 CHANGELOG = f"{GITHUB}/blob/master/CHANGELOG.md"
 
+INSTALL_REQUIRES = [
+    "starlette>=0.11, <0.12",
+    "uvicorn>=0.5.1, <0.6",
+    "jinja2>=2.10.1",
+    "whitenoise",
+    "requests",
+    "parse",
+    "python-multipart",
+    "websockets>=6.0",
+    "aiodine>=1.2.1, <2.0",
+]
+
+EXTRAS_REQUIRE = {"files": ["aiofiles"], "sessions": ["itsdangerous"]}
+EXTRAS_REQUIRE["full"] = [
+    req for reqs in EXTRAS_REQUIRE.values() for req in reqs
+]
+
 setuptools.setup(
     name="bocadillo",
     version="0.13.2",
@@ -19,20 +36,10 @@ setuptools.setup(
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=["bocadillo"],
+    packages=setuptools.find_packages(),
     package_data={"bocadillo": ["assets/*"]},
-    install_requires=[
-        "starlette>=0.11, <0.12",
-        "uvicorn>=0.5.1, <0.6",
-        "jinja2>=2.10.1",
-        "whitenoise",
-        "requests",
-        "parse",
-        "python-multipart",
-        "websockets>=6.0",
-        "aiodine>=1.2.1, <2.0",
-    ],
-    extras_require={"files": ["aiofiles"], "sessions": ["itsdangerous"]},
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
     python_requires=">=3.6",
     url=DOCS,
     project_urls={
